f5c4ab2cb3d1b83384116a4357a5701d5f1acfd1
Alexey Savchenko <nahalem@ukr.net>
2018-10-05 17:02:11 +0300

    Initial Commit

---
+++ NetworkingExample-Medium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingExample-Medium
+//
+//  Created by Alexey Savchenko on 10/5/18.
+//  Copyright © 2018 Alexey Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
